6a36176b89f98e4c5a37e852f4ed44eb8ef84415
ppgod123 <3343674462@qq.com>
2022-01-20 10:50:33 +0800

    2022年1月20日10:50:29提交

--- main/文件和异常/remember_me.py
+++ main/文件和异常/remember_me.py
@@ -6,9 +6,53 @@
  @模块备注:
 """
 import json
+import sys
 
-username = raw_input("请输入您的姓名：")
-filename = 'username.json'
-with open(filename,'w') as f_obj:
-    json.dump(username,f_obj,ensure_ascii=False)
-    print("欢迎进入我们的系统:"+username+"!!!")
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+# username = raw_input("请输入您的姓名：")
+# filename = 'username.json'
+# with open(filename,'w') as f_obj:
+#     json.dump(username,f_obj,ensure_ascii=False)
+#     print("欢迎进入我们的系统:"+username+"!!!")
+
+#重构
+def greet_user():
+    """问候用户并指出其名字"""
+    filename = 'username.json'
+    try:
+     with open(filename) as f_obj:
+         username = json.load(f_obj)
+    except IOError:
+        username = raw_input("Q亲输入您的姓名:")
+        with open(filename,'w') as f_obj:
+            json.dump(username,f_obj,ensure_ascii=False)
+            print("我们将记住你！！！"+username)
+    else:
+        print ("欢迎归来，"+username +"!")
+
+def get_stored_username():
+    """若存储了用户，就获取它"""
+    filename = 'username.json'
+    try:
+        with open(filename) as f_obj:
+            username = json.load(f_obj)
+    except IOError:
+        return None
+    else:
+        return username
+
+def get_new_username():
+    """提示用户输入用户名"""
+    username = raw_input("请输入您的姓名:")
+    filename = 'username.json'
+    with open(filename,'w') as f_obj:
+        json.dump(username,f_obj)
+    return username
+
+
+if __name__ == '__main__':
+    greet_user()
+    print (get_stored_username())
+    print (get_new_username())
---
+++ main/文件和异常/test01.py
@@ -0,0 +1,11 @@
+#-*- coding: UTF-8 -*-
+"""
+ @编写人:冯爱军
+ @开发时间:2022/1/20 10:32
+ @功能模块:
+ @模块备注:
+"""
+import remember_me
+
+remember_me.greet_user()
+print(remember_me.get_stored_username())
